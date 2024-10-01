ea462911c5f660b80140f421e393bc42d7842d8e
Sergei Kiryutkin <kiryutkin88@yandex.ru>
2024-10-01 12:25:53 +0300

    formated code

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,7 @@ const cardTemplate = document.querySelector('#card-template').content;
 const cardList = document.querySelector('.places__list');
 const buttonAddCard = document.querySelector('.profile__add-button');
 // @todo: Функция создания карточки
-function createCard (cardData, cardDelete) {
+function createCard(cardData, cardDelete) {
     const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
     const cardImage = cardElement.querySelector('.card__image');
     const cardTitle = cardElement.querySelector('.card__title');
@@ -28,6 +28,6 @@ function cardRender(container, cardData) {
     container.append(cardData);
 };
 // @todo: Вывести карточки на страницу
-initialCards.forEach(function(obj) {
+initialCards.forEach(function (obj) {
     cardRender(cardList, createCard(obj, cardDelete));
 });
