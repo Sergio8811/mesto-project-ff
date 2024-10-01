35249caa8d89aaf23fd5dd721bc1b35dcf3ae3bf
Sergei Kiryutkin <kiryutkin88@yandex.ru>
2024-10-01 12:14:19 +0300

    complete

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,33 @@
 // @todo: Темплейт карточки
-
+const cardTemplate = document.querySelector('#card-template').content;
 // @todo: DOM узлы
-
+const cardList = document.querySelector('.places__list');
+const buttonAddCard = document.querySelector('.profile__add-button');
 // @todo: Функция создания карточки
+function createCard (cardData, onDelete) {
+    const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
+    const cardImage = cardElement.querySelector('.card__image');
+    const cardTitle = cardElement.querySelector('.card__title');
 
+    cardImage.src = cardData.link;
+    cardImage.alt = cardData.name;
+
+    cardTitle.textContent = cardData.name;
+
+    cardElement.querySelector('.card__delete-button').addEventListener('click', () => {
+        onDelete(cardElement);
+    });
+    return cardElement;
+};
 // @todo: Функция удаления карточки
+function onDelete(cardElement) {
+    cardElement.remove();
+};
 
+function cardRender(container, cardData) {
+    container.append(cardData);
+};
 // @todo: Вывести карточки на страницу
+initialCards.forEach(function(obj) {
+    cardRender(cardList, createCard(obj, onDelete));
+});
